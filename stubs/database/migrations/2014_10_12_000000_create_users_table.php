596d32db44dbdc92a1adbe5f0dbf4f5ca9a7fa83
queensherainfotech <queensherainfotech@gmail.com>
2023-06-01 23:31:56 +0530

    removal of default value of photo in users table

--- stubs/database/migrations/2014_10_12_000000_create_users_table.php
+++ stubs/database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->text('two_factor_secret')->nullable();
             $table->text('two_factor_recovery_codes')->nullable();
             $table->timestamp('two_factor_confirmed_at')->nullable();
-            $table->string('photo')->nullable()->default('https://ui-avatars.com/api/?name=Q&color=7F9CF5&background=EBF4FF');
+            $table->string('photo')->nullable();
             $table->string('role')->nullable();
             $table->string('googleId')->nullable();
             $table->string('fcmToken')->nullable();
